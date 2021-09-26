b9f37165b945b407c8f01352309a76094a9db277
haejii <yanghagi@naver.com>
2021-09-26 23:29:24 +0900

    Chang Root Controller

--- InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
+++ InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -5,4 +5,17 @@
 //  Created by 양혜지 on 2021/09/26.
 //
 
-import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+}
--- InstagramFirestoreTutorial/Controller/Authentication/RegistratopmController.swift
+++ InstagramFirestoreTutorial/Controller/Authentication/RegistratopmController.swift
@@ -5,4 +5,18 @@
 //  Created by 양혜지 on 2021/09/26.
 //
 
-import Foundation
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+}
