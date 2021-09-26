8fbdbc74d34fa7d42c28ca1d72bce0b2f10e68b1
haejii <yanghagi@naver.com>
2021-09-26 23:35:21 +0900

    Implement Gradien in Login Page

--- InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
+++ InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -14,8 +14,20 @@ class LoginController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: -Helpers
+    
+    func configureUI(){
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
         
-        view.backgroundColor = .systemPink
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
     }
 
 }
